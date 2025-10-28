adfae2f5d14af3d556af5a67796111d6e4760701
Logan Meyers <theterrarian03@gmail.com>
2025-10-27 18:39:55 -0700

    Definitely not just starting now

---
+++ include/amazonInventorySystem.hpp
@@ -0,0 +1,7 @@
+// manager for data
+// holds container for avlMap for storing inventory items
+// holds hash set container for very fast searching
+    // std::hash
+    // quadratic probing
+    // dynamically allocated array for storage, based on categories
+// function for reading in csv data
